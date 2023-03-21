9acae55389cef654147f6426a28a14b56347337c
Vladimir <vovakovalev2016@icloud.com>
2023-03-21 18:46:15 +0300

    initial commit

---
+++ GitHubFollowers/Controller/SignInController.swift
@@ -0,0 +1,15 @@
+//
+//  SignInController.swift
+//  GitHubFollowers
+//
+//  Created by Vladimir Kovalev on 21.03.2023.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
